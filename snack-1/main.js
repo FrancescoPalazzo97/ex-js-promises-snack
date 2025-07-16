ae79d6f6a8358ebffd5e2c2b398f6861cae6871b
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-07-16 15:30:25 +0200

    UPD - Snack 1 bonus done

--- snack-1/main.js
+++ snack-1/main.js
@@ -1,19 +1,34 @@
 const API_POST = `https://dummyjson.com/posts/`;
 const API_USERS = `https://dummyjson.com/users/`;
 
-const getPostTitle = id => {
-    return new Promise((res, rej) => {
+const getPost = id => {
+    return new Promise((resolve, reject) => {
         fetch(`${API_POST}${id}`)
-            .then(response => response.json())
-            .then(post => res(post.title))
-            .catch(rej)
+            .then(res => res.json())
+            .then(post => {
+                fetch(`${API_USERS}${post.userId}`)
+                    .then(res => res.json())
+                    .then(user => {
+                        const newObj = {
+                            ...post,
+                            user
+                        }
+                        resolve(newObj)
+                    })
+            })
+            .catch(reject)
     })
 }
 
-getPostTitle(1)
-    .then(title => console.log(`Titolo: ${title}`))
-    .catch(err => console.error(err))
+// const getPostTitle = id => {
+//     return new Promise((res, rej) => {
+//         fetch(`${API_POST}${id}`)
+//             .then(response => response.json())
+//             .then(post => res(post.title))
+//             .catch(rej)
+//     })
+// }
 
-getPostTitle(2)
-    .then(title => console.log(`Titolo: ${title}`))
+getPost(1)
+    .then(post => console.log(`Post completo:`, post))
     .catch(err => console.error(err))
